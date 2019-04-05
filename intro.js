0a8d478a221a9833c9f6af284d0c6d96ea4b3188
Sergiu Matei <matei83@gmail.com>
2019-04-04 23:26:16 -0500

    new

--- intro.js
+++ intro.js
@@ -21,9 +21,6 @@ var color = getColor();
 summaryElement.style.color = color;
 
 
-
-
-
 function hidePage(page) {
 	var el = document.getElementById(page);
 	el.style.display = 'none';
@@ -31,3 +28,29 @@ function hidePage(page) {
 function showPage(page) {
 	document.getElementById(page).style.display = 'block';
 }
+
+function initMenu() {
+	var links = document.querySelectorAll("#top-menu-bar a");
+	console.info(links);
+	for (var i = 0; i < links.length; i++) {
+			links[i].onclick = clickOnMenuItem;
+	}
+}
+
+function clickOnMenuItem () {
+	hideAllPages ();
+	var pageId = this.getAttribute('data-page');
+	showPage (pageId);
+}
+
+
+function hideAllPages () {
+	var pages = document.querySelectorAll ('.page');
+	for (var i = 0; i < pages.length; i++) {
+		pages[i].style.display = 'none';
+	}
+
+}
+
+initMenu();
+
